041fa62cb16437066d849c55fda0abc45512bbcf
Junaid Salmani <junaidsalmani95@gmail.com>
2021-11-25 23:13:31 +0530

    Just for practicing

---
+++ index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Growth track</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <?//php include_once 'include/header.php';  ?>
+        <section class="banner">
+            <img src="img/logo.png">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="inner-content">
+                            <h2>Be the <br>
+                                technology <br>
+                                Disruptor <br>
+                                With <br>
+                                <span class="text-danger">M.tech in </span> <br>
+                            </h2>
+                            <div class="pt-xl-4 pt-md-3">
+                                <p>Autonomous</p>
+                                <p>Electrical Vehicles</p>
+                            </div>
+                            <div class="pt-xl-3 pt-md-2">
+                                <p>Computer Aided</p>
+                                <p>Structural Design</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6 col-lg-3 col-xl-3 form-part">
+                        <form class="">
+                            <h5 class="font-weight-bold text-center text-danger">ENQUIRY NOW</h5>
+                            <div class="form-group mb-2">
+                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name">
+                            </div>
+                            <div class="form-group mb-2">
+                                <input type="email" class="form-control" id="exampleInputPassword1" placeholder="E-email">
+                            </div>
+                            <div class="form-group mb-2">
+                                <input type="tel" class="form-control" id="exampleInputPassword1" placeholder="Mobile Number">
+                            </div>
+                            <div class="form-group mb-2">
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>Select Course</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                            <div class="form-group mb-2">
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>Select State</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input mt-2" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1"><small>I agree to receive information</small></label>
+                            </div>
+                            <h6 class="my-3 font-weight-bold">Existing User? <span class="float-right">LOGIN</span></h6>
+                            <button type="submit" class="btn btn-dark btn-block text-uppercase">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-2">
+            <div class="container">
+                <div class="row inner-student justify-content-end">
+                    <div class="col-md-4 bg-img">
+                        <div class="student-text">
+                            <h2>MU SIGNIFIES <br>
+                                THE '<span class="font-weight-bold">rise</span> <br>
+                                <span class="font-weight-bold">of the new</span> <br>
+                                <span class="font-weight-bold">Engineer'</span> 
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <p>An Extremly paraduigm made possible by a 
+                            <span class="font-weight-bold">historic colloboration involving the Tech Mahindra of the Mahindra Group, Ecole Central Paris (now CentraleSupelec)</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="specialization">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md">
+                        <h2 class="text-center">M.TECH SPECIALIZATIONS</h2>
+                        <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Autonomous Eletric Vehicles</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Computer-Aided Structural Engg.</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                            <div class=" tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-md-9  p-4">
+                                            <h3 class="text-danger py-3">Autonomous Eletric Vehicles</h3>
+                                            <p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                                <span class="font-weight-bold">tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</span>
+                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                                consequat.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-md-9 p-4">
+                                            <h3 class="text-danger py-3">Computer Aided Structural Engineering</h3>
+                                            <p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                                <span class="font-weight-bold">tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</span>
+                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                                consequat.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="edge">
+            <h4 class="mt-5 mb-5 text-center">THE MAHINDRA EDGE</h4>
+            <div class="container">
+            	<div class="row justify-content-center">
+            		<div class="col-md-10">
+            			<div class="row justify-content-center">
+            				<div class="col-md">
+            					<h4>Autonomous Electric Vehicles</h4>
+            					<ul>
+            						<li>
+            							<span>Advance learning & research</span>in Elecetri vehicle Engineering
+            						</li>
+            						<li>
+            							<span>Advance learning & research</span>in Elecetri vehicle Engineering
+            						</li>
+            						<li>
+            							<span>Advance learning & research</span>in Elecetri vehicle Engineering
+            						</li>
+            						<li>
+            							<span>Advance learning & research</span>in Elecetri vehicle Engineering
+            						</li>
+            						<li>
+            							<span>Advance learning & research</span>in Elecetri vehicle Engineering
+            						</li>
+            					</ul>
+            				</div>
+            				<div class="col-md">
+            
+            					<h4>Autonomous Electric Vehicles</h4>
+            					<ul>
+            						<li>
+            							<span>Advance learning & research</span>in Elecetri vehicle Engineering
+            						</li>
+            						<li>
+            							<span>Advance learning & research</span>in Elecetri vehicle Engineering
+            						</li>
+            						<li>
+            							<span>Advance learning & research</span>in Elecetri vehicle Engineering
+            						</li>
+            						<li>
+            							<span>Advance learning & research</span>in Elecetri vehicle Engineering
+            						</li>
+            						<li>
+            							<span>Advance learning & research</span>in Elecetri vehicle Engineering
+            						</li>
+            					</ul>
+            
+            				</div>
+            			</div>
+            		</div>
+            	</div>
+            </div>
+            </section>
+        <section class="py-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-9 mt-5">
+                        <h2 class="text-center font-weight-bold">STIPEND</h2>
+                        <p class="text-center mt-lg-3">ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            <span class="text-danger font-weight-bold">incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="new-age-learning">
+            <div class="container bg-color-new">
+                <div class="row justify-content-end">
+                    <img src="img/global-edge.png">
+                    <div class="col-md-10 new-age-learning-content">
+                        <h2 class="text-center mt-5 font-weight-bold">NEW AGE LEARNING</h2>
+                        <p class="text-center">MU partnered with some of the world's leading institution to <br> impart education with global edge</p>
+                        <div class="d-flex bd-highlight mb-3">
+                            <div class=" flex-shrink-1 bd-highlight">
+                                <div class="my-2 ">
+                                    <h6>UNIQUE</h6>
+                                    <h6>PEDAGIOGICAL</h6>
+                                    <h6>METHOD</h6>
+                                </div>
+                            </div>
+                            <div class="p-2 w-100 bd-highlight">
+                                <p>We are counted among industry leaders to provide website design to users. Designs are designed to express professional services.>We are counted among industry leaders to provide website design to users. Designs are designed to express professional services.</p>
+                            </div>
+                        </div>
+                        <div class="d-flex bd-highlight  mb-3">
+                            <div class=" flex-shrink-1 bd-highlight">
+                                <div class="my-2 ">
+                                    <h6>INDUSTRY</h6>
+                                    <h6>SPONSORSHIPS</h6>
+                                    <h6> INTERNSHIPS</h6>
+                                </div>
+                            </div>
+                            <div class="p-2 w-100 bd-highlight">
+                                <p>We are counted among industry leaders to provide website design to users. Designs are designed to express professional services.>We are counted among industry leaders to provide website design to users. Designs are designed to express professional services.</p>
+                            </div>
+                        </div>
+                        <div class="d-flex bd-highlight  mb-3">
+                            <div class=" flex-shrink-1 bd-highlight">
+                                <div class="my-2 pr-4">
+                                    <h6>INNOVATIVE</h6>
+                                    <h6>TEACHING</h6>
+                                    <h6>AMBIENCE</h6>
+                                </div>
+                            </div>
+                            <div class="p-2 w-100 bd-highlight">
+                                <p>We are counted among industry leaders to provide website design to users. Designs are designed to express professional services.>We are counted among industry leaders to provide website design to users. Designs are designed to express professional services.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
